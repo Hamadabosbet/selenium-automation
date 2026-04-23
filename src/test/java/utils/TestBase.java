59851f88d6c4afb6ed76ea4d89e9ebc82d101d09
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2026-04-24 01:29:59 +0300

    Add screenshot capture on test failure in TestBase class

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -1,8 +1,16 @@
 package utils;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Base class for test classes providing common setup and teardown functionality.
@@ -29,7 +37,19 @@ public class TestBase {
      * Tears down the WebDriver after each test method.
      */
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            try {
+                TakesScreenshot ts = (TakesScreenshot) driver;
+                File source = ts.getScreenshotAs(OutputType.FILE);
+                String screenshotPath = "screenshots/" + result.getName() + ".png";
+                Files.createDirectories(Paths.get("screenshots"));
+                Files.copy(source.toPath(), Paths.get(screenshotPath));
+                System.out.println("Screenshot saved: " + screenshotPath);
+            } catch (IOException e) {
+                System.out.println("Failed to capture screenshot: " + e.getMessage());
+            }
+        }
         DriverFactory.quitDriver();
     }
 }
