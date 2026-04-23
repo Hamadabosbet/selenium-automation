bbd24bf41c96bd4ad096a1bb5f0d7cda137c7028
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2026-04-24 01:30:43 +0300

    Add method to retrieve H1 title from HomePage

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -8,6 +8,7 @@ public class HomePage {
 
     private By logo = By.cssSelector("img[src*='logo_store_bright']");
     private WebDriver driver;
+    private By h1Title = By.tagName("h1");
 
     public  HomePage(WebDriver driver) {
         this.driver = driver;
@@ -19,6 +20,11 @@ public class HomePage {
     public boolean isLogoDisplayed() {
         WebElement logoElement = driver.findElement(logo);
         return logoElement.isDisplayed();
+    }
+    public  String getH1Title() {
+        WebElement h1Element= driver.findElement(h1Title);
+        return h1Element.getText();
+
     }
 
 }
