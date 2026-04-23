72f29b883be6dc0585d4739e6fae0f887ab82ed5
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2026-04-24 02:12:54 +0300

    Enhance HomePage class with explicit wait for H1 title and add method to retrieve H1 element

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,30 +1,42 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class HomePage {
 
     private By logo = By.cssSelector("img[src*='logo_store_bright']");
     private WebDriver driver;
     private By h1Title = By.tagName("h1");
+    private WebDriverWait wait;
 
     public  HomePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public String getPageTitle() {
         return driver.getTitle();
     }
+
     public boolean isLogoDisplayed() {
         WebElement logoElement = driver.findElement(logo);
         return logoElement.isDisplayed();
     }
     public  String getH1Title() {
-        WebElement h1Element= driver.findElement(h1Title);
+        WebElement h1Element= wait.until(ExpectedConditions.visibilityOfElementLocated(h1Title));
         return h1Element.getText();
 
     }
+    public WebElement getH1Element() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(h1Title));
+    }
+
 
 }
