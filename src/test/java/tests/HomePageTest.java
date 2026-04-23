9fbe4c72cb251df2786fe89bab742f0a03b937d9
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2026-04-24 02:46:32 +0300

    Add test to verify navigation links on HomePage

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -5,6 +5,9 @@ import org.testng.annotations.Test;
 import pages.HomePage;
 import utils.TestBase;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class HomePageTest  extends TestBase {
     private static final String EXPECTED_TITLE = "ATID Demo Store – ATID College Demo Store for Practicing QA Automation";
 
@@ -33,5 +36,18 @@ public class HomePageTest  extends TestBase {
         }
 
 
+    }
+
+    @Test
+    public void verifyNavigationLinks() {
+        HomePage homePage = new HomePage(driver);
+
+        List<String> expectedLinks = Arrays.asList(
+                "HOME", "STORE", "MEN", "WOMEN", "ACCESSORIES", "ABOUT", "CONTACT US"
+        );
+
+        List<String> actualLinks = homePage.getNavLinksText();
+        System.out.println(actualLinks);
+        Assert.assertEquals(actualLinks, expectedLinks, "Navigation links are incorrect!");
     }
 }
