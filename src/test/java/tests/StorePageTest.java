9b0d67be41fe263ff8bd973932f2583be3efebf7
Hamadabosbet <73755871+Hamadabosbet@users.noreply.github.com>
2026-04-24 16:23:11 +0300

    Add test to verify product details are displayed in StorePageTest

--- src/test/java/tests/StorePageTest.java
+++ src/test/java/tests/StorePageTest.java
@@ -7,23 +7,31 @@ import utils.TestBase;
 public class StorePageTest extends TestBase {
 
     @Test
-    public  void verifyProductsAreDisplayed(){
-        StorePage storePage= new StorePage(driver);
+    public void verifyProductsAreDisplayed() {
+        StorePage storePage = new StorePage(driver);
         storePage.goToStorePage();
         storePage.areAllProductsDisplayed();
     }
 
 
     @Test
-    public void verifyProductCount(){
-        StorePage storePage=new StorePage(driver);
+    public void verifyProductCount() {
+        StorePage storePage = new StorePage(driver);
         storePage.goToStorePage();
-        int actualProductCount=storePage.getProductCount();
-        int expectedProductCount=12;
-        assert actualProductCount==expectedProductCount:"Expected "+expectedProductCount+" products but found "+actualProductCount;
+        int actualProductCount = storePage.getProductCount();
+        int expectedProductCount = 12;
+        assert actualProductCount == expectedProductCount : "Expected " + expectedProductCount + " products but found " + actualProductCount;
 
 
     }
 
+    @Test
+    public void verifyProductDetailsAreDisplayed() {
+        StorePage storePage = new StorePage(driver);
+        storePage.goToStorePage();
+        assert storePage.areAllProductsDetialDisplayed() : "All product details are not displayed correctly : " ;
 
+
+
+    }
 }
